6fe53141b9d8c3b858936f550995794632b3bee0
Kaci Oussama <kacioussama0@gmail.com>
2026-02-16 01:33:51 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -205,7 +205,7 @@ useFetch("/api/wc/products", {
 
                     <p class="mb-4 w-50 text-light  d-none d-lg-block">NIOH 3 | PS5 Préparez-vous à affronter les ténèbres une nouvelle fois avec Nioh 3, l’évolution ultime du "Masocore" de la Team NINJA, exclusivement conçue pour la PlayStation 5. Plongez dans une version fantastique et brutale de l’histoire …</p>
 
-                    <RouterLink  :to="'/shop/product/' + product.slug" class="btn btn-outline-light mb-5">ACHETEZ MAINTENANT</RouterLink>
+                    <RouterLink  :to="'/shop/product/' + product.slug" class="btn btn-outline-light rounded-0 mb-5">ACHETEZ MAINTENANT</RouterLink>
 
                   </div>
 
