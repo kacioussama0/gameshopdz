d61ec0c89f45012278961bf84c26500cfe9349c4
Kaci Oussama <kacioussama0@gmail.com>
2026-02-18 03:41:04 +0100

    update-config

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
+process.env.NITRO_PORT ??= process.env.PORT;      // Hostinger provides PORT
+process.env.NITRO_HOST ??= '0.0.0.0';             // listen publicly
+
 import './.output/server/index.mjs'
