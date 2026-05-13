b07c945a39e6f41fddd627be34d6acf5cf4fd411
kacioussama0 <test@Oussama-Macbook.local>
2026-05-13 15:31:48 +0100

    fix-view

--- app.vue
+++ app.vue
@@ -5,6 +5,14 @@ const { refresh } = useWcCart()
 
 const { itemsCount} = useWcCart()
 
+useHead({
+  meta: [
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1, maximum-scale=1",
+    },
+  ],
+});
 
 onMounted(() => {
 
